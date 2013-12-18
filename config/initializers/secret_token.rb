209eb77495078dbc738d410fb4c6b5e86a9c338e
Aarvin M Sasidharan <aarvinms@gmail.com>
2013-12-18 10:25:52 +0000

    Created a rails application and added category

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopping::Application.config.secret_token = '6c763a851241dd367dcbb87a6155824fbfdb9753b65de1c20fdb61c536415d3de895d20a9c1d6c646e5d42253dd70158d2ad16526235b8c2d92691f4efd25ce0'
